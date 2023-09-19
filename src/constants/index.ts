e5a67e04a178558440ea644c8179afc26d302bb3
Fernando Parra <fernandoparra@live.com>
2023-09-19 03:08:34 +0200

    chore: 24hs cache

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,5 +3,5 @@ export const API_URL = process.env.REACT_APP_API_URL;
 
 export const CACHE_PODCASTS_KEY = 'podcasts';
 export const CACHE_EPISODES_KEY = 'episodes';
-// export const CACHE_TIME = 1000 * 60 * 60 * 24;
-export const CACHE_TIME = 1000 * 60;
+export const CACHE_TIME = 1000 * 60 * 60 * 24;
+// export const CACHE_TIME = 1000 * 60;
